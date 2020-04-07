408e0d755f32196f854b278c3eec2ba8a03b2401
jewelvictoria <55025198+jewelvictoria@users.noreply.github.com>
2020-04-07 09:14:56 +0800

    Add files via upload

--- login_customer.php
+++ login_customer.php
@@ -13,7 +13,6 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<script src="http://use.edgefonts.net/bebas-neue:n4:default;montserrat:n4:default.js" type="text/javascript"></script>
 </head>
-
 <header>
 </header>
 <br> 
@@ -27,7 +26,7 @@
 	<button onclick="window.location.href = 'lookup_order.php';">Look Up Order</button>
 </div>
 <br><br>
-<form action="new_transaction.php" method="post" class="form-horizontal">
+<form action="new_transaction.php" method="post" class="form-horizontal" onsubmit="return Validate()" name="vform">
 	<section style="margin-left: 20%; margin-right: 20%;">
         <fieldset class="border p-2">
             <h4>
@@ -35,26 +34,80 @@
                     Register Information
                 </div>
             </h4>
-
             <div class="form-group">
-                Name: <input type="text" name="customer_name" class="form-control" placeholder="Juan Dela Cruz" size="25" maxlength="30" required>
-            </div>
-
+                Name: <input type="text" name="customer_name" class="form-control" placeholder="Juan Dela Cruz" size="25" maxlength="30">
+				<div id="name_error"></div>
+			</div>
             <div class="form-group">
-                Contact Number: <input type="tel" name="contact_number" class="form-control" placeholder="09XXXXXXXXX" size="25" minlength="11" maxlength="11" required>
-            </div>
+                Contact Number: <input type="tel" name="contact_number" class="form-control" placeholder="09XXXXXXXXX" size="25" minlength="9" maxlength="11">
+				<div id="num_error"></div>
+			</div>
 		</fieldset>
 	</section>
 	<div class="form-group">
-		<input type="submit" onclick="check(this.form)"></input>
+		<input type="submit" name="register"></input>
 	</div>
 </form>		
-	<script language="javascript">
-            function check(form) { 
-                    window.open('branding.php',"_self")
-                    alert("Successfully Registered!")
-                
-            }
+	<script type="text/javascript">
+		//GET ALL INPUT TEXT OBJECTS
+		var customer_name = document.forms["vform"]["customer_name"];
+		var contact_number = document.forms["vform"]["contact_number"];
+		var num = /^[0-9]+$/;
+		
+		//GET ALL ERROR DISPLAY OBJECTS
+		var name_error = document.getElementById("name_error");
+		var num_error = document.getElementById("num_error");
+		
+		// SETTING ALL EVENT LISTENERS
+		customer_name.addEventListener('blur', nameVerify, true);
+		contact_number.addEventListener('blur', numVerify, true);
+		
+		// function validation
+		function Validate() {
+		  if (customer_name.value == "") {
+			customer_name.style.border = "1px solid red";
+			document.getElementById('name_error').style.color = "red";
+			name_error.textContent = "*Name is required";
+			customer_name.focus();
+			return false;
+		  }
+		  if (contact_number.value == "") {
+			contact_number.style.border = "1px solid red";
+			document.getElementById('num_error').style.color = "red";
+			num_error.textContent = "*Contact number is required";
+			contact_number.focus();
+			return false;
+		  }
+		  if(contact_number.value.match(num)){
+			  window.open('branding.php',"_self")
+              alert("Successfully Registered!")
+		  }
+		  else{
+			  contact_number.style.border = "1px solid red";
+			  document.getElementById('num_error').style.color = "red";
+			  num_error.textContent = "*All inputs should be in numbers";
+			  contact_number.focus();
+			  return false;
+		  }
+		}
+		
+		//EVENT HANDLER FUNCTIONS
+		function nameVerify(){
+			if (customer_name.value != ""){
+				customer_name.style.border = "1px solid #5E6E66";
+				name_error.innerHTML = "";
+				return true;
+			}
+		}
+		function numVerify(){
+			if (contact_number.value != ""){
+				contact_number.style.border = "1px solid #5E6E66";
+				num_error.innerHTML = "";
+				return true;
+			}
+		}
+		
+		
 	</script>	
 	
 <br>
